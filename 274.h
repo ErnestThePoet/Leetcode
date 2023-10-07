79230c940244dfabd323c00ef47f24731929d3ce
Ernest Cui <ecuiships@126.com>
2023-10-07 17:48:45 +0800

    chore: add 274

---
+++ 274.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int hIndex(vector<int>& citations)
+    {
+        sort(citations.begin(), citations.end(), [](int a, int b) { return a > b; });
+
+        for (int i = 0; i < citations.size(); i++)
+        {
+            if (citations[i] < i + 1)
+            {
+                return i;
+            }
+        }
+
+        return citations.size();
+    }
+};
